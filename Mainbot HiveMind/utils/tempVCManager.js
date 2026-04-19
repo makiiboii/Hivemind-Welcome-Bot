11448e9bda3e0accbed27e1c93f60b9e0d1c5621
makiboii11 <jmsipalay06@gmail.com>
2026-04-19 23:38:10 +0800

    Delete Mainbot HiveMind directory

--- Mainbot HiveMind/utils/tempVCManager.js
+++
@@ -1,9 +0,0 @@
-// Temp VC Manager - Handles temporary voice channel creation and management
-
-const tempVCs = new Map(); // channelId -> ownerId
-const sentHelp = new Set(); // Track help messages sent
-
-module.exports = {
-  tempVCs,
-  sentHelp,
-};
